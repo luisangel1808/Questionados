b8c22854abe3df3fa19beb4c59e437ae7992cb70
luisangel1808 <luisangel1808@hotmail.com>
2021-01-23 19:26:55 -0500

    versón lista 1.0

--- index.js
+++ index.js
@@ -7,63 +7,67 @@ class Game{
         this.right=0;
         this.incorrect=0;
         this.totalQuestions=20;
-        this.getData()
-        this.showElements()
+        this.seconds=0;
+        this.minutes=0;
+        this.active=true;
+        this.getData();
+        this.showElements();
+        this.timer();
     }
 
     showElements(){
-        const loginContainer=document.getElementById('login')
-        loginContainer.classList.replace('login','hidden')
-        const gameContainer=document.getElementById('game')
-        gameContainer.classList.replace('hidden','question')
-        const corrects = document.getElementById('corrects')
-        corrects.innerHTML='0'
-        const incorrects = document.getElementById('incorrects')
-        incorrects.innerHTML='0'
+        const loginContainer=document.getElementById('login');
+        loginContainer.classList.replace('login','hidden');
+        const gameContainer=document.getElementById('game');
+        gameContainer.classList.replace('hidden','question');
+        const corrects = document.getElementById('corrects');
+        corrects.innerHTML='0';
+        const incorrects = document.getElementById('incorrects');
+        incorrects.innerHTML='0';
     }
 
     showQuestion(question){        
         if(this.number===this.totalQuestions){
-            prompt(`Has terminado`)
-            this.showTable()
+            this.active=false;
+            alert(`Has terminado`);
+            this.showTable();
         }
         else{
-            const questionsContainer=document.querySelector('.questions')
-            this.removeElements(questionsContainer)
-            const title = document.createElement('h2')
-            title.textContent='Selecciona la opción correcta'        
-            questionsContainer.appendChild(title) 
-            const questionTitle = document.createElement('h3')
-            questionTitle.innerText=question.Question        
-            const optionsContainer=document.createElement('div')
-            optionsContainer.classList.add('options')
-            questionsContainer.appendChild(optionsContainer)
-            let options=(question.Options)
-            options.push(`${question.Correct}`)
-            options.sort(()=>{return 0.5 - Math.random()})
+            const questionsContainer=document.querySelector('.questions');
+            this.removeElements(questionsContainer);
+            const title = document.createElement('h2');
+            title.textContent='Selecciona la opción correcta';    
+            questionsContainer.appendChild(title); 
+            const questionTitle = document.createElement('h3');
+            questionTitle.innerText=question.Question;        
+            const optionsContainer=document.createElement('div');
+            optionsContainer.classList.add('options');
+            questionsContainer.appendChild(optionsContainer);
+            let options=(question.Options);
+            options.push(`${question.Correct}`);
+            options.sort(()=>{return 0.5 - Math.random()});
             options.map((option)=>{
-                const optionButton = document.createElement('Button')
-                optionButton.type='button'
-                optionButton.textContent=option
-                optionsContainer.appendChild(optionButton)
+                const optionButton = document.createElement('Button');
+                optionButton.type='button';
+                optionButton.textContent=option;
+                optionsContainer.appendChild(optionButton);
                 if(option===question.Correct){
                     optionButton.onclick = ()=>{
-                        this.goodAnswer()
-                        this.showQuestion(this.questionsSelected[this.number])
+                        this.goodAnswer();
+                        this.showQuestion(this.questionsSelected[this.number]);
                     }
                 }
                 else{
                     optionButton.onclick = ()=>{
-                        this.badAnswer()
-                        this.showQuestion(this.questionsSelected[this.number])
+                        this.badAnswer();
+                        this.showQuestion(this.questionsSelected[this.number]);
                     }
                 }
     
             })    
-            console.log(this.number)
             this.showImage();
-            questionsContainer.insertBefore(questionTitle,optionsContainer) 
-            this.number++
+            questionsContainer.insertBefore(questionTitle,optionsContainer); 
+            this.number++;
         }
 
     }
@@ -87,12 +91,22 @@ class Game{
         this.right++
         const corrects = document.getElementById('corrects')
         corrects.innerHTML=`${this.right}`
+        const gAMessagges= ['Lo haces genial','¡Que inteligente!','¡Eres genial!','¡Sigue así!','¡Fantástico!']
+        this.showMessage(gAMessagges)
+
     }
     badAnswer(){
 
         this.incorrect++
         const incorrects = document.getElementById('incorrects')
         incorrects.innerHTML=`${this.incorrect}`
+        const bAMessagges= ['Uy, casi','Lo siento, incorrecto','Concéntrate un poco más', 'Fallaste por poco', 'Sigue intentando']
+        this.showMessage(bAMessagges)
+    }
+
+    showMessage(phrases){
+        const messagge = document.getElementById('message')
+        messagge.innerText=phrases[Math.round(Math.random()*5)]       
     }
     showImage(){
         const image=document.querySelector('#questionImg') 
@@ -116,13 +130,12 @@ class Game{
         tdScore.innerText=`${this.right}/${this.incorrect}`
         tRow.appendChild(tdScore)
         const tdTime = document.createElement('td')
-        tdTime.innerText=`${minutesBoard}:${secondsBoard}`
+        tdTime.innerText=`${this.minutesBoard}:${this.secondsBoard}`
         tRow.appendChild(tdTime)
         const scoresContainer=document.getElementById('scores')
         scoresContainer.classList.replace('hidden','scores')
         const gameContainer=document.getElementById('game')
         gameContainer.classList.replace('question','hidden')
-
         const playAgain = document.getElementById('playAgain')
         playAgain.onclick = () =>{
             scoresContainer.classList.replace('scores','hidden')
@@ -130,51 +143,33 @@ class Game{
             loginContainer.classList.replace('hidden','login')
         }
     }   
-      
+
+    timer(){
+        
+            setInterval(() => {
+                if (this.active===true) {
+                    const timerContainer=document.getElementById('timer')
+                    this.seconds++;
+                    if (this.seconds === 60) {
+                    this.seconds = 0;
+                    this.minutes++;
+                    }
+                    this.secondsBoard = `${this.seconds}`;
+                    this.minutesBoard = `${this.minutes}`;
+                    if (this.seconds < 10) {
+                    this.secondsBoard = `0${this.seconds}`;
+                    }
+                    if (this.minutes < 10) {
+                    this.minutesBoard = `0${this.minutes}`;
+                    }
+                    timerContainer.innerHTML = `${this.minutesBoard}:${this.secondsBoard}`;
+                }
+            },1000);       
+    };
 }  
 
 function start(category){    
     const playerInput = document.getElementById('player')
     const player = playerInput.value;
-    game = new Game(category,player)
-    timer(band)
-    seconds=0;
-    minutes=0;
-    secondsBoard=0; 
-    minutesBoard=0;
+    const game = new Game(category,player)
 }
-
-let seconds=0;
-let minutes=0;
-let secondsBoard=0; 
-let minutesBoard=0;
-let band=true
-function timer(){
-    if(band){
-        setInterval(this.setTimer, 1000);
-        band=false
-    }     
-};
-  
-function setTimer(){
-    
-    const timerContainer=document.getElementById('timer')
-    seconds++;
-    if (seconds === 60) {
-      seconds = 0;
-      minutes++;
-    }
-    secondsBoard = `${seconds}`;
-    minutesBoard = `${minutes}`;
-    if (seconds < 10) {
-      secondsBoard = `0${seconds}`;
-    }
-    if (minutes < 10) {
-      minutesBoard = `0${minutes}`;
-    }
-    timerContainer.innerHTML = `${minutesBoard}:${secondsBoard}`;
-  };
-
-
-const goodAnswerMessagges= ['Lo haces genial','¡Que inteligente!','¡Eres genial!','¡Sigue así!','¡Fantástico!']
-const badAnswerMessagges= ['Uy, casi','Lo siento, incorrecto','Concéntrate un poco más', 'Fallaste por poco', 'Sigue intentando']
